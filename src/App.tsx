22d0b1193fdde8a55c0c0539eb5e035e3a172c83
syed <user.email>
2022-09-26 08:56:22 +0100

    dloaded data + rendering new compact data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { greet } from "./utils/greet";
+import countriesData from "./data/allCountriesData.json"
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <div>      
+      {countriesData.map((countryData):JSX.Element => {
+        return (
+          <p key={countryData.name.common}>{`{ country: ${countryData.name.common}, capital: ${countryData.capital} }`}</p>
+        )
+      })}      
+    </div>
+  );
 }
 
 export default App;
