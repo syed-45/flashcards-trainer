e15c79b012de2d951fce353057a56732f77803ee
syed <user.email>
2022-10-03 08:02:38 +0100

    created start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Question } from "./Components/Question";
-import { RevealAnswer } from "./Components/RevealAnswer";
-import countries_capitals from "./data/countries&capitals.json"
+// import { Question } from "./Components/Question";
+// import { RevealAnswer } from "./Components/RevealAnswer";
+// import countries_capitals from "./data/countries&capitals.json"
+import "./App.css"
 
 function App(): JSX.Element {
   return (
-    <div>
-      <Question/>
-      <RevealAnswer/>
+    <div className='start-screen'>
+      <h1 className='app-title'>CAPITALS FLASHCARDS</h1>
+      <button className="start-btn">START</button>
+      {/* <Question/>
+      <RevealAnswer/> */}
       {/* {countries_capitals.map((country)=><p key={country.country}>{country.country}</p>)} */}
     </div>
   );
