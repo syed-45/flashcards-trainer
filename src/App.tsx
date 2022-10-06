a39c10df755609d67767e6622d99707631ee69a4
syed <user.email>
2022-10-06 09:25:52 +0100

    created props for Question comp + useState for screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-// import { Question } from "./Components/Question";
+import { Question } from "./Components/Question";
 // import { RevealAnswer } from "./Components/RevealAnswer";
 // import countries_capitals from "./data/countries&capitals.json"
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 
 function App(): JSX.Element {
+  const [screen, setScreen] = useState<string>("start");
   return (
-    <div className='start-screen'>
-      <h1 className='app-title'>CAPITALS FLASHCARDS</h1>
-      <button className="start-btn">START</button>
-      {/* <Question/>
-      <RevealAnswer/> */}
+    <div>
+      {screen === "start" && (
+        <div className="start-screen">
+          <h1 className="app-title">CAPITALS FLASHCARDS</h1>
+          <button className="start-btn">START</button>
+        </div>
+      )}
+      {screen === "question" && <Question setScreen={setScreen} country="wasifcountry" />}
+      {/* {screen === "answer" && <RevealAnswer/>} */}
       {/* {countries_capitals.map((country)=><p key={country.country}>{country.country}</p>)} */}
     </div>
   );
--- src/Components/Question.tsx
+++ src/Components/Question.tsx
@@ -1,7 +1,8 @@
-export function Question():JSX.Element {
-    return (
-    <div>
-        The question !!!
-    </div>
-    )
+interface QuestionProps {
+  country: string;
+  setScreen: React.Dispatch<React.SetStateAction<string>>;
+}
+export function Question(props: QuestionProps): JSX.Element {
+  // = showCountry()
+  return <div>{props.country + "!!!!!"}</div>;
 }
