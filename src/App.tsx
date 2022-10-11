7a2cc498d3442872fda2e615fa15a0894c937e8a
syed <user.email>
2022-10-11 19:07:13 +0100

    layout done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { Question } from "./Components/Question";
-// import { RevealAnswer } from "./Components/RevealAnswer";
 // import countries_capitals from "./data/countries&capitals.json"
+import { Question } from "./Components/Question";
+import { RevealAnswer } from "./Components/RevealAnswer";
 import { useState } from "react";
 import "./App.css";
 
 function App(): JSX.Element {
-  const [screen, setScreen] = useState<string>("start");
+  const [screen, setScreen] = useState<string>("question");
   return (
     <div>
       {screen === "start" && (
-        <div className="start-screen">
+        <div className="main-screen">
           <h1 className="app-title">CAPITALS FLASHCARDS</h1>
           <button className="start-btn">START</button>
         </div>
       )}
       {screen === "question" && <Question setScreen={setScreen} country="wasifcountry" />}
-      {/* {screen === "answer" && <RevealAnswer/>} */}
+      {screen === "answer" && <RevealAnswer/>}
       {/* {countries_capitals.map((country)=><p key={country.country}>{country.country}</p>)} */}
     </div>
   );
--- src/Components/Question.tsx
+++ src/Components/Question.tsx
@@ -4,5 +4,13 @@ interface QuestionProps {
 }
 export function Question(props: QuestionProps): JSX.Element {
   // = showCountry()
-  return <div>{props.country + "!!!!!"}</div>;
+  return (
+    <div className='main-screen'>
+        <h1 className="country">{props.country + "!!!!!"}</h1>
+        <div className="question-screen-btns">
+            <button onClick={()=>console.log('need another random country!')}>know</button>
+            <button onClick={()=>console.log('hadnle know click!')}>don't know</button>
+        </div>
+    </div>
+  );
 }
