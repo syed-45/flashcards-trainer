bf88ab6684da8bb749aa3980f15f4035f15f7601
Syed <haque_45@hotmail.co.uk>
2022-10-12 09:19:56 +0100

    finished basic layout

--- src/Components/RevealAnswer.tsx
+++ src/Components/RevealAnswer.tsx
@@ -1,3 +1,13 @@
-export function RevealAnswer(): JSX.Element {
-  return <div>TTHis is the answer!</div>;
+interface RevealAnswerProps {  
+  capital: string;
+  setScreen: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export function RevealAnswer(props: RevealAnswerProps): JSX.Element {
+  return (
+  <div className="main-screen">
+    <h1 className="capital">{props.capital}</h1>
+    <button onClick={() => props.setScreen("question")}>NEXT</button>        
+  </div>
+  );
 }
